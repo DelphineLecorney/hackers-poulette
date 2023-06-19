a81862ba61baec179a1932c19c316b45c1fdb000
DelphineLecorney <lecorney.delphine@gmail.com>
2023-06-19 10:20:44 +0200

    Begin form

--- index.php
+++ index.php
@@ -1,8 +1,6 @@
 <?php
 include('./assets/php/connect.php');
 
-
-
 ?>
 
 <!DOCTYPE html>
@@ -13,6 +11,28 @@ include('./assets/php/connect.php');
     <title>Contact Support</title>
 </head>
 <body>
-    
+    <h1>Contact form</h1>
+    <form id="AddData" method="POST" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+
+        <div class="form-group">
+            <label for="name">Name :</label>
+            <input type="text" id="name" name="name" value="<?php echo $name; ?>" required>
+            <span class="error"><?php echo $nameError; ?></span>
+        </div>
+        <br>
+        <div class="form-group">
+            <label for="firstname">Firstname :</label>
+            <input type="text" id="firstname" name="firstname" value="<?php echo $firstname; ?>" required>
+            <span class="error"><?php echo $firstnameError; ?></span>
+        </div>
+        <br>
+        <div class="form-group">
+            <label for="addressEmail">Address e-mail :</label>
+            <input type="text" id="addressEmail" name="addressEmail" value="<?php echo $addressEmail; ?>" required>
+            <span class="error"><?php echo $addressEmailError; ?></span>
+        </div>
+        <br>
+
+    </form>
 </body>
 </html>
