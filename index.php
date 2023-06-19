da86aca71b6a555d497bcfe293c67b476d12948c
DelphineLecorney <lecorney.delphine@gmail.com>
2023-06-19 09:47:55 +0200

    Add files

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+include('./assets/php/connect.php');
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contact Support</title>
+</head>
+<body>
+    
+</body>
+</html>
